daec0bfd0fd19eef34c8d87903036574c50bdba9
Sebastian Barry <61334340+sebastianbarry@users.noreply.github.com>
2022-03-13 16:22:00 -0600

    Create Movies.js

---
+++ Movies.js
@@ -0,0 +1,22 @@
+let envPath = __dirname + "/../.env"
+require('dotenv').config({path:envPath});
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+mongoose.Promise= global.Promise;
+
+try {
+    mongoose.connect( process.env.DB, {useNewUrlParser: true, useUnifiedTopology: true}, () =>
+        console.log("connected"));
+}catch (error) {
+    console.log("could not connect");
+}
+mongoose.set('useCreateIndex', true);
+
+var MovieSchema = new Schema({
+    title:{type:String,required:true,index:{unique:true}},
+    yearReleased:{type:Date, required:true},
+    genre:{type:String,required:true,enum:['Action', 'Adventure', 'Comedy', 'Drama', 'Fantasy', 'Horror', 'Mystery', 'Thriller', 'Western'] },
+    actors: { type: [{actorName: String, characterName: String}], required: true }
+});
+module.exports = mongoose.model('Movie', MovieSchema);
